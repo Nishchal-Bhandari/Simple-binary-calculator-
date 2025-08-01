576bd362dc5f7128ed5bdaaed7595128b80ae6ed
Nishchal-Bhandari <nishchalbhandari18@gmail.com>
2025-08-01 11:30:03 +0530

    added python file

---
+++ simple _calculater.py
@@ -0,0 +1,47 @@
+# Simple-binary-calculator-
+print("\t\t\tSIMPLE CALCULATOR \n\n")
+print("This is a binary calculator💻. you can perform binary operations using it.\n")
+print("Note:\n   \t you can perform +,-,×,÷\n")
+print(".........................................")
+
+equ=input()
+
+mlen=len(equ)
+elen=len(equ)-1  
+
+if (equ[mlen-1])=="=":
+   if ("+"in equ)==True:
+     opn="+"
+   elif ("-"in equ)==True:
+       opn="-"
+   elif ("×"in equ)==True:
+       opn="×"
+   elif ("÷"in equ)==True:
+       opn="÷"
+   else:
+        print("INVALID OPERATION 😭 ")
+        exit()
+    
+
+   for i in range(elen):
+       if equ[i]==opn:
+         loc=i
+
+   opd1=int(equ[0:loc])
+   opd2=int(equ[loc+1:mlen-1])
+
+
+   if opn=="+":
+      res=opd1+opd2
+   elif opn=="-":
+       res=opd1-opd2
+   elif opn=="×":
+       res=opd1*opd2
+   elif opn=="÷":
+       res=opd1/opd2
+    
+    
+   print(res)
+
+else:
+    print("INVALID OPERATION 😭 ")
